82d98c07847cb14e7079bf1ee2c5067aa386270a
vendar <eplaylity@gmail.com>
2012-12-03 17:25:24 +0800

    1) use black/white list, and ip list to limit access clients. 2) use redis record client access of hostname 3) use log4js record client access of url

--- index.js
+++ index.js
@@ -3,18 +3,27 @@ var http = require('http'),
     irule = require('./irule');
 
 var proxyserver = http.createServer(function(clientreq,clientres){
-  var clienturl = url.parse(clientreq.url);
+  var cip = clientreq.socket.remoteAddress;
+  if(clientreq.headers.hasOwnProperty('x-forwarded-for')){
+    cip = clientreq.headers['x-forwarded-for'];
+  }
+  if(!irule.ipfilter(cip)){
+    clientres.writeHead(403);
+    return clientres.end();
+  }
 
-  var reqhostname = irule.transfilter(clienturl.hostname);
-  if(!reqhostname){
-    console.warn('DENIED: %s',clienturl.hostname);
+  var clienturl = url.parse(clientreq.url);
+  var requrl = irule.filter(clienturl.host+clienturl.path);
+
+  if(!requrl){
+    console.warn('[DENY] - %s%s - %s',clienturl.host,clienturl.path,cip);
     clientres.writeHead(404);
     return clientres.end();
   }
 
   var proxyreqopts = {
-    //ost: clienturl.host,
-    hostname: reqhostname,
+    host: clienturl.host,
+    hostname: clienturl.hostname,
     port: clienturl.port?clienturl.port:80,
     path: clienturl.path,
     method: clientreq.method,
@@ -24,13 +33,12 @@ var proxyserver = http.createServer(function(clientreq,clientres){
   var t = Date.now();
   var l = 0;
   function end(){
-    irule.trace(
+    irule.footprint(
       proxyreqopts.hostname,
       clienturl.pathname,
       clienturl.query,
       proxyreqopts.headers.cookie,
-      l,
-      Date.now()-t);
+      cip, l, Date.now()-t);
     clientres.end();
   }
   var proxyreq = http.request(proxyreqopts,function(proxyres){
--- irule.js
+++ irule.js
@@ -1,62 +1,136 @@
-var fs = require('fs');
+var fs = require('fs'),
+    log4js = require('log4js'),
+    redis = require('redis').createClient(18088);
 
-var leaklist = [];
-var transtable = {};
-
-function load_transtable(){
-  leaklist = [];
-  transtable = {};
-  fs.readFileSync(__dirname+'/conf/transtable.ip','utf8')
-    .split('\n')
-    .filter(function(line){return line.length;})
-    .map(function(line){
-      var segs = line.split(' ').filter(function(el){return el.length});
-      if(segs.length===0){
-        return false;
-      }
-      var p1 = segs[0].trim();
-      var p2 = segs.length>=2?segs[1].trim():false;
-      switch(p1){
-      case '-':
-        transtable[p2] = false;
-        break;
-      case '+':
-        leaklist.push(RegExp(p2));
-        break;
-      default:
-        transtable[p1] = p2;
-        break;
-      }
-      return true;
-    });
-
-  console.log('-- leak list --');
-  console.log(leaklist);
-  console.log('-- transtable --');
-  console.log(transtable);
+exports.conf = conf = {};
+try{
+  conf = JSON.parse(fs.readFileSync(__dirname+'/conf/iproxy.json'));
+}catch(error){
+  console.error(error);
+  process.exit(-1);
 }
 
-load_transtable();
+log4js.loadAppender('file');
+log4js.loadAppender('multiprocess');
+log4js.clearAppenders();
+log4js.addAppender(log4js.appenders.multiprocess({
+  mode: 'master',
+  actualAppender: log4js.appenders.file(conf.log.path)
+}),'iproxy');
+exports.logger = logger = log4js.getLogger('iproxy');
+logger.setLevel(conf.log.level);
 
-exports.trace = function trace(hostname,pathname,parameters,cookie,bodylen,usedtime){
-  console.log('%s%s%s - %d/%d',hostname,pathname?pathname:'',parameters?parameters:'',bodylen,usedtime/1000);
+var whitelist = [];
+var blacklist = [];
+var iplist = [];
+
+var whitelistfile = __dirname+'/conf/whitelist';
+var blacklistfile = __dirname+'/conf/blacklist';
+var iplistfile = __dirname+'/conf/iplist';
+
+var update_blacklist_timeout = null;
+var update_whitelist_timeout = null;
+var update_iplist_timeout = null;
+
+var update_delay = 500;
+
+fs.watchFile(whitelistfile,function(curr,prev){
+  if(update_whitelist_timeout){
+    clearTimeout(update_whitelist_timeout);
+  }
+  update_whitelist_timeout = setTimeout(function(){
+    update_whitelist();
+  },update_delay);
+});
+
+fs.watchFile(blacklistfile,function(curr,prev){
+  if(update_blacklist_timeout){
+    clearTimeout(update_blacklist_timeout);
+  }
+  update_blacklist_timeout = setTimeout(function(){
+    update_blacklist();
+  },update_delay);
+});
+
+fs.watchFile(iplistfile,function(curr,prev){
+  if(update_iplist_timeout){
+    clearTimeout(update_iplist_timeout);
+  }
+  update_iplist_timeout = setTimeout(function(){
+    update_iplist();
+  },update_delay);
+});
+
+function loadRegExps(filename){
+  return fs.readFileSync(filename,'utf8')
+    .split('\n')
+    .filter(function(line){return line.length>1&&line[0]!=='#'})
+    .map(function(line){return RegExp('^'+line+'.*')});
 };
 
-exports.transfilter = function transfilter(srcurl){
+function update_blacklist(){
+  blacklist = loadRegExps(blacklistfile);
+  logger.trace('---- update blacklist');
+  logger.trace(blacklist);
+};
+
+function update_whitelist(){
+  whitelist = loadRegExps(whitelistfile);
+  logger.trace('---- update whitelist');
+  logger.trace(whitelist);
+};
+
+function update_iplist(){
+  iplist = loadRegExps(iplistfile);
+  logger.trace('---- update iplist');
+  logger.trace(iplist);
+};
+
+update_blacklist();
+update_whitelist();
+update_iplist();
+
+exports.footprint = function footprint(hostname,pathname,parameters,cookie,clientip,bodylen,usedtime){
+  logger.trace('[PROXY] - %s%s%s - %s/%d/%d',
+    hostname,
+    pathname?pathname:'',
+    parameters?parameters:'',
+    clientip,
+    bodylen,
+    usedtime/1000);
+  redis.hincrby(clientip,hostname,1,function(error,res){
+    if(error){
+      logger.error(error);
+    }
+  });
+};
+
+exports.filter = function filter(srcurl){
   if(!srcurl){
     return false;
   }
-  for(var i=0;i<leaklist.length;++i){
-    if(leaklist[i].test(srcurl)){
+  for(var i=0;i<whitelist.length;++i){
+    if(whitelist[i].test(srcurl)){
       return srcurl;
     }
   }
-  for(var k in transtable){
-    var r = RegExp(k);
-    if(r.test(srcurl)){
-      return transtable[k];
+  for(var i=0;i<blacklist.length;++i){
+    if(blacklist[i].test(srcurl)){
+      return false;
     }
   }
   return srcurl;
+};
+
+exports.ipfilter = function ipfilter(ip){
+  if(!ip){
+    return false;
+  }
+  for(var i=0;i<iplist.length;++i){
+    if(iplist[i].test(ip)){
+      return true;
+    }
+  }
+  return false;
 };
 
